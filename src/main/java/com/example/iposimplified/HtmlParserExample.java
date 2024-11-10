6a67fceec610e693b28c5c06e4eacf09adb18ec8
rohitth bandaru <f20190541@hyderabad.bits-pilani.ac.in>
2024-11-10 19:25:15 +0530

    wip

--- src/main/java/com/example/iposimplified/HtmlParserExample.java
+++ src/main/java/com/example/iposimplified/HtmlParserExample.java
@@ -2,6 +2,9 @@ package com.example.iposimplified;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,13 +16,22 @@ public class HtmlParserExample {
             Document doc = Jsoup.connect("https://ipowatch.in/ipo-grey-market-premium-latest-ipo-gmp/").get();
             Element[] table = doc.getElementsByClass("wp-block-table is-style-regular has-medium-font-size").first().child(0).child(0).children().toArray(new Element[0]);
             for(int i=1; i<table.length; i++) {
-                String link = table[i].child(0).child(0).attr("href");
-                System.out.println("link : " + link);
-                System.out.println("name : " + table[i].child(0).child(0).text());
-                System.out.println("dates : " + table[i].child(0).child(2).text());
-                System.out.println("ipo gmp : "+ table[i].child(1).text());
-                System.out.println("per share price : " + table[i].child(2).text());
-                System.out.println("issue type : " + table[i].child(6).text());
+                HashMap<String, String> details = new HashMap<String, String>();
+                details.put("link", table[i].child(0).child(0).attr("href"));
+                details.put("name", table[i].child(0).child(0).text());
+                details.put("gmp", table[i].child(1).text() );
+                details.put("issue_type", table[i].child(6).text());
+
+                Document ipo = Jsoup.connect(details.get("link")).get();
+                Element[] ipo_details = ipo.getElementsByClass("wp-block-table is-style-stripes has-medium-font-size").first().child(0).child(0).children().toArray(new Element[0]);
+                for(int j=1; j<ipo_details.length; j++) {
+                    details.put(ipo_details[j].child(0).text(), ipo_details[j].child(1).text());
+                }
+                Element[] ipo_dates = ipo.getElementsByClass("wp-block-table is-style-stripes has-medium-font-size").toArray(new Element[0])[2].child(0).child(0).children().toArray(new Element[0]);
+                for(int j=1; j<ipo_dates.length; j++) {
+                    details.put(ipo_dates[j].child(0).text(), ipo_dates[j].child(1).text());
+                }
+
                 System.out.println("#############################################################################");
             }
         } catch (IOException e) {
